933b42ace74107076250a3e88ceeafa959ff8746
olofsj <olofsj@gmail.com>
2011-11-10 10:11:33 +0100

    Improve the admin lists.

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -7,12 +7,19 @@ class ProjectImageInline(admin.StackedInline):
 
 
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('name', 'tagline', 'category', 'featured')
+    list_display = ('name', 'tagline', 'category', 'featured', 'site_link')
+    search_fields = ('name', 'tagline', 'short_description', 'description',
+            'category__name')
+    ordering = ('name',)
     prepopulated_fields = {'slug': ('name',)}
     inlines = [
         ProjectImageInline,
     ]
     exclude = ('rendered_description',)
+
+    def site_link(self, obj):
+        return '<a href="%s">View on site</a>' % obj.get_absolute_url()
+    site_link.allow_tags = True
 
     def set_featured(self, request, queryset):
         queryset.update(featured=True)
@@ -26,12 +33,22 @@ class ProjectAdmin(admin.ModelAdmin):
 
 
 class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'project_count', 'projects', 'site_link')
+    search_fields = ('name', 'short_description', 'description')
     prepopulated_fields = {'slug': ('name',)}
-    list_display = ('name', 'project_count')
 
     def project_count(self, obj):
         return str(obj.project_set.count())
     project_count.short_description = 'Projects'
+
+    def projects(self, obj):
+        return '<a href="../project/?category=%d">View projects</a>' % obj.id
+    projects.short_description = 'View Projects'
+    projects.allow_tags = True
+
+    def site_link(self, obj):
+        return '<a href="%s">View on site</a>' % obj.get_absolute_url()
+    site_link.allow_tags = True
 
 
 admin.site.register(Project, ProjectAdmin)
